0f8ac88b5b3b27bf2c0cc7b2e84990ec65ef4afd
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2024-03-16 18:58:31 +0100

    update for latest mruby-tls

--- src/mrb_wslay.c
+++ src/mrb_wslay.c
@@ -16,10 +16,10 @@ mrb_wslay_event_on_msg_recv_callback(wslay_event_context_ptr ctx,
     mrb->jmp = &c_jmp;
 
     mrb_value argv[4];
-    argv[0] = mrb_fixnum_value(arg->rsv);
-    argv[1] = MRB_GET_OPCODE(mrb_fixnum_value(arg->opcode));
+    argv[0] = mrb_int_value(mrb, arg->rsv);
+    argv[1] = MRB_GET_OPCODE(mrb_int_value(mrb, arg->opcode));
     argv[2] = mrb_str_new(mrb, (const char *) arg->msg, arg->msg_length);
-    argv[3] = MRB_GET_STATUSCODE(mrb_fixnum_value(arg->status_code));
+    argv[3] = MRB_GET_STATUSCODE(mrb_int_value(mrb, arg->status_code));
 
     mrb_value on_msg_recv_arg = mrb_obj_new(mrb,
       mrb_class_get_under(mrb,
@@ -57,22 +57,33 @@ mrb_wslay_event_recv_callback(wslay_event_context_ptr ctx,
 
     mrb_value argv[2];
     argv[0] = mrb_cptr_value(mrb, buf);
-    argv[1] = mrb_fixnum_value(len);
+    argv[1] = mrb_int_value(mrb, len);
 
     errno = 0;
     mrb_assert(mrb_type(data->recv_callback) == MRB_TT_PROC);
     mrb_value buf_obj = mrb_yield_argv(mrb, data->recv_callback, NELEMS(argv), argv);
 
-    if (mrb_fixnum_p(buf_obj)) {
-      ret = mrb_fixnum(buf_obj);
-    } else {
-      buf_obj = mrb_str_to_str(mrb, buf_obj);
-      ret = RSTRING_LEN(buf_obj);
-      if (ret < 0||ret > len) {
-        mrb_raise(mrb, E_RANGE_ERROR, "returned buf doesn't fit");
-      }
-      if (ret > 0) {
-        memmove(buf, (uint8_t *) RSTRING_PTR(buf_obj), ret);
+    switch (mrb_type(buf_obj)) {
+      case MRB_TT_INTEGER:
+        ret = mrb_fixnum(buf_obj);
+        break;
+      case MRB_TT_SYMBOL: {
+        mrb_sym want = mrb_symbol(buf_obj);
+        if (want == mrb_intern_lit(mrb, "tls_want_pollin") || want == mrb_intern_lit(mrb, "tls_want_pollout"))
+          wslay_event_set_error(ctx, WSLAY_ERR_WOULDBLOCK);
+        else
+          goto def;
+      } break;
+      default: {
+def:
+        buf_obj = mrb_str_to_str(mrb, buf_obj);
+        ret = RSTRING_LEN(buf_obj);
+        if (ret < 0||ret > len) {
+          mrb_raise(mrb, E_RANGE_ERROR, "returned buf doesn't fit");
+        }
+        if (ret > 0) {
+         memmove(buf, (uint8_t *) RSTRING_PTR(buf_obj), ret);
+        }
       }
     }
 
@@ -112,16 +123,23 @@ mrb_wslay_event_send_callback(wslay_event_context_ptr ctx,
   struct mrb_jmpbuf c_jmp;
   mrb_int ret = -1;
   MRB_TRY(&c_jmp) {
-    data->mrb->jmp = &c_jmp;
+    mrb->jmp = &c_jmp;
 
     errno = 0;
     mrb_value buf_obj = mrb_str_new(mrb, (const char *) buf, len);
     mrb_assert(mrb_type(data->send_callback) == MRB_TT_PROC);
     mrb_value sent = mrb_yield(mrb, data->send_callback, buf_obj);
-
-    ret = mrb_int(mrb, sent);
-
-    mrb_assert(ret >= 0&&ret <= len);
+    switch (mrb_type(sent)) {
+      case MRB_TT_INTEGER:
+        ret = mrb_int(mrb, sent);
+        mrb_assert(ret >= 0&&ret <= len);
+        break;
+      case MRB_TT_SYMBOL: {
+        mrb_sym want = mrb_symbol(sent);
+        if (want == mrb_intern_lit(mrb, "tls_want_pollin") || want == mrb_intern_lit(mrb, "tls_want_pollout"))
+          wslay_event_set_error(ctx, WSLAY_ERR_WOULDBLOCK);
+      } break;
+    }
 
     mrb->jmp = prev_jmp;
   } MRB_CATCH(&c_jmp) {
@@ -203,7 +221,7 @@ mrb_wslay_event_recv(mrb_state *mrb, mrb_value self)
     mrb_exc_raise(mrb, mrb_obj_value(mrb->exc));
   }
   else if (err != 0) {
-    return MRB_WSLAY_ERROR(mrb_fixnum_value(err));
+    return MRB_WSLAY_ERROR(mrb_int_value(mrb, err));
   }
 
   return self;
@@ -223,7 +241,7 @@ mrb_wslay_event_send(mrb_state *mrb, mrb_value self)
     mrb_exc_raise(mrb, mrb_obj_value(mrb->exc));
   }
   else if (err != 0) {
-    return MRB_WSLAY_ERROR(mrb_fixnum_value(err));
+    return MRB_WSLAY_ERROR(mrb_int_value(mrb, err));
   }
 
   return self;
@@ -265,7 +283,7 @@ mrb_wslay_event_queue_msg(mrb_state *mrb, mrb_value self)
     mrb_raise(mrb, E_WSLAY_ERROR, "the given message is invalid");
   }
   else if (err != 0) {
-    return MRB_WSLAY_ERROR(mrb_fixnum_value(err));
+    return MRB_WSLAY_ERROR(mrb_int_value(mrb, err));
   }
 
   return self;
@@ -296,7 +314,7 @@ mrb_wslay_event_queue_close(mrb_state *mrb, mrb_value self)
     mrb_raise(mrb, E_WSLAY_ERROR, "the given message is invalid");
   }
   else if (err != 0) {
-    return MRB_WSLAY_ERROR(mrb_fixnum_value(err));
+    return MRB_WSLAY_ERROR(mrb_int_value(mrb, err));
   }
 
   return self;
@@ -329,25 +347,25 @@ mrb_wslay_event_get_close_sent(mrb_state *mrb, mrb_value self)
 static mrb_value
 mrb_wslay_event_get_status_code_received(mrb_state *mrb, mrb_value self)
 {
-  return MRB_GET_STATUSCODE(mrb_fixnum_value(wslay_event_get_status_code_received(((mrb_wslay_user_data *) DATA_PTR(self))->ctx)));
+  return MRB_GET_STATUSCODE(mrb_int_value(mrb, wslay_event_get_status_code_received(((mrb_wslay_user_data *) DATA_PTR(self))->ctx)));
 }
 
 static mrb_value
 mrb_wslay_event_get_status_code_sent(mrb_state *mrb, mrb_value self)
 {
-  return MRB_GET_STATUSCODE(mrb_fixnum_value(wslay_event_get_status_code_sent(((mrb_wslay_user_data *) DATA_PTR(self))->ctx)));
+  return MRB_GET_STATUSCODE(mrb_int_value(mrb, wslay_event_get_status_code_sent(((mrb_wslay_user_data *) DATA_PTR(self))->ctx)));
 }
 
 static mrb_value
 mrb_wslay_event_get_queued_msg_count(mrb_state *mrb, mrb_value self)
 {
-  return mrb_fixnum_value(wslay_event_get_queued_msg_count(((mrb_wslay_user_data *) DATA_PTR(self))->ctx));
+  return mrb_int_value(mrb, wslay_event_get_queued_msg_count(((mrb_wslay_user_data *) DATA_PTR(self))->ctx));
 }
 
 static mrb_value
 mrb_wslay_event_get_queued_msg_length(mrb_state *mrb, mrb_value self)
 {
-  return mrb_fixnum_value(wslay_event_get_queued_msg_length(((mrb_wslay_user_data *) DATA_PTR(self))->ctx));
+  return mrb_int_value(mrb, wslay_event_get_queued_msg_length(((mrb_wslay_user_data *) DATA_PTR(self))->ctx));
 }
 
 static mrb_value
@@ -392,7 +410,7 @@ mrb_wslay_event_context_server_init(mrb_state *mrb, mrb_value self)
     mrb_exc_raise(mrb, mrb_obj_value(mrb->nomem_err));
   }
   else if (err != 0) {
-    return MRB_WSLAY_ERROR(mrb_fixnum_value(err));
+    return MRB_WSLAY_ERROR(mrb_int_value(mrb, err));
   }
 
   return self;
@@ -440,7 +458,7 @@ mrb_wslay_event_context_client_init(mrb_state *mrb, mrb_value self)
     mrb_exc_raise(mrb, mrb_obj_value(mrb->nomem_err));
   }
   else if (err != 0) {
-    return MRB_WSLAY_ERROR(mrb_fixnum_value(err));
+    return MRB_WSLAY_ERROR(mrb_int_value(mrb, err));
   }
 
   return self;
@@ -453,7 +471,7 @@ mrb_wslay_get_rsv1(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "i", &rsv);
 
-  return mrb_fixnum_value(wslay_get_rsv1(rsv));
+  return mrb_int_value(mrb, wslay_get_rsv1(rsv));
 }
 
 static mrb_value
@@ -463,7 +481,7 @@ mrb_wslay_get_rsv2(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "i", &rsv);
 
-  return mrb_fixnum_value(wslay_get_rsv2(rsv));
+  return mrb_int_value(mrb, wslay_get_rsv2(rsv));
 }
 
 static mrb_value
@@ -473,7 +491,7 @@ mrb_wslay_get_rsv3(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "i", &rsv);
 
-  return mrb_fixnum_value(wslay_get_rsv3(rsv));
+  return mrb_int_value(mrb, wslay_get_rsv3(rsv));
 }
 
 void
@@ -488,15 +506,15 @@ mrb_mruby_wslay_gem_init(mrb_state* mrb) {
   wslay_error_cl = mrb_define_class_under(mrb, wslay_mod, "Err", E_RUNTIME_ERROR);
   mrb_value wslay_error_hash = mrb_hash_new_capa(mrb, 9 * 2);
   mrb_define_const(mrb, wslay_mod, "Error", wslay_error_hash);
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_WANT_READ), mrb_symbol_value(mrb_intern_lit(mrb, "want_read")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_WANT_WRITE), mrb_symbol_value(mrb_intern_lit(mrb, "want_write")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_PROTO), mrb_symbol_value(mrb_intern_lit(mrb, "proto")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_INVALID_ARGUMENT), mrb_symbol_value(mrb_intern_lit(mrb, "invalid_argument")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_INVALID_CALLBACK), mrb_symbol_value(mrb_intern_lit(mrb, "invalid_callback")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_NO_MORE_MSG), mrb_symbol_value(mrb_intern_lit(mrb, "no_more_msg")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_CALLBACK_FAILURE), mrb_symbol_value(mrb_intern_lit(mrb, "callback_failure")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_WOULDBLOCK), mrb_symbol_value(mrb_intern_lit(mrb, "wouldblock")));
-  mrb_hash_set(mrb, wslay_error_hash, mrb_fixnum_value(WSLAY_ERR_NOMEM), mrb_symbol_value(mrb_intern_lit(mrb, "nomem")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_WANT_READ), mrb_symbol_value(mrb_intern_lit(mrb, "want_read")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_WANT_WRITE), mrb_symbol_value(mrb_intern_lit(mrb, "want_write")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_PROTO), mrb_symbol_value(mrb_intern_lit(mrb, "proto")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_INVALID_ARGUMENT), mrb_symbol_value(mrb_intern_lit(mrb, "invalid_argument")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_INVALID_CALLBACK), mrb_symbol_value(mrb_intern_lit(mrb, "invalid_callback")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_NO_MORE_MSG), mrb_symbol_value(mrb_intern_lit(mrb, "no_more_msg")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_CALLBACK_FAILURE), mrb_symbol_value(mrb_intern_lit(mrb, "callback_failure")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_WOULDBLOCK), mrb_symbol_value(mrb_intern_lit(mrb, "wouldblock")));
+  mrb_hash_set(mrb, wslay_error_hash, mrb_int_value(mrb, WSLAY_ERR_NOMEM), mrb_symbol_value(mrb_intern_lit(mrb, "nomem")));
   mrb_value wslay_error_hash_keys = mrb_hash_keys(mrb, wslay_error_hash);
   for (mrb_int i = 0; i < RARRAY_LEN(wslay_error_hash_keys); i++) {
     mrb_value key = mrb_ary_ref(mrb, wslay_error_hash_keys, i);
@@ -506,18 +524,18 @@ mrb_mruby_wslay_gem_init(mrb_state* mrb) {
 
   mrb_value wslay_status_code_hash = mrb_hash_new_capa(mrb, 12 * 2);
   mrb_define_const(mrb, wslay_mod, "StatusCode", wslay_status_code_hash);
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_NORMAL_CLOSURE), mrb_symbol_value(mrb_intern_lit(mrb, "normal_closure")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_GOING_AWAY), mrb_symbol_value(mrb_intern_lit(mrb, "going_away")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_PROTOCOL_ERROR), mrb_symbol_value(mrb_intern_lit(mrb, "protocol_error")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_UNSUPPORTED_DATA), mrb_symbol_value(mrb_intern_lit(mrb, "unsupported_data")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_NO_STATUS_RCVD), mrb_symbol_value(mrb_intern_lit(mrb, "no_status_rcvd")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_ABNORMAL_CLOSURE), mrb_symbol_value(mrb_intern_lit(mrb, "abnormal_closure")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_INVALID_FRAME_PAYLOAD_DATA), mrb_symbol_value(mrb_intern_lit(mrb, "invalid_frame_payload_data")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_POLICY_VIOLATION), mrb_symbol_value(mrb_intern_lit(mrb, "policy_violation")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_MESSAGE_TOO_BIG), mrb_symbol_value(mrb_intern_lit(mrb, "message_too_big")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_MANDATORY_EXT), mrb_symbol_value(mrb_intern_lit(mrb, "mandatory_ext")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_INTERNAL_SERVER_ERROR), mrb_symbol_value(mrb_intern_lit(mrb, "internal_server_error")));
-  mrb_hash_set(mrb, wslay_status_code_hash, mrb_fixnum_value(WSLAY_CODE_TLS_HANDSHAKE), mrb_symbol_value(mrb_intern_lit(mrb, "tls_handshake")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_NORMAL_CLOSURE), mrb_symbol_value(mrb_intern_lit(mrb, "normal_closure")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_GOING_AWAY), mrb_symbol_value(mrb_intern_lit(mrb, "going_away")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_PROTOCOL_ERROR), mrb_symbol_value(mrb_intern_lit(mrb, "protocol_error")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_UNSUPPORTED_DATA), mrb_symbol_value(mrb_intern_lit(mrb, "unsupported_data")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_NO_STATUS_RCVD), mrb_symbol_value(mrb_intern_lit(mrb, "no_status_rcvd")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_ABNORMAL_CLOSURE), mrb_symbol_value(mrb_intern_lit(mrb, "abnormal_closure")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_INVALID_FRAME_PAYLOAD_DATA), mrb_symbol_value(mrb_intern_lit(mrb, "invalid_frame_payload_data")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_POLICY_VIOLATION), mrb_symbol_value(mrb_intern_lit(mrb, "policy_violation")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_MESSAGE_TOO_BIG), mrb_symbol_value(mrb_intern_lit(mrb, "message_too_big")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_MANDATORY_EXT), mrb_symbol_value(mrb_intern_lit(mrb, "mandatory_ext")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_INTERNAL_SERVER_ERROR), mrb_symbol_value(mrb_intern_lit(mrb, "internal_server_error")));
+  mrb_hash_set(mrb, wslay_status_code_hash, mrb_int_value(mrb, WSLAY_CODE_TLS_HANDSHAKE), mrb_symbol_value(mrb_intern_lit(mrb, "tls_handshake")));
   mrb_value wslay_status_code_hash_keys = mrb_hash_keys(mrb, wslay_status_code_hash);
   for (mrb_int i = 0; i < RARRAY_LEN(wslay_status_code_hash_keys); i++) {
     mrb_value key = mrb_ary_ref(mrb, wslay_status_code_hash_keys, i);
@@ -527,17 +545,17 @@ mrb_mruby_wslay_gem_init(mrb_state* mrb) {
 
   mrb_value io_flags_hash = mrb_hash_new_capa(mrb, 2);
   mrb_define_const(mrb, wslay_mod, "IoFlags", io_flags_hash);
-  mrb_hash_set(mrb, io_flags_hash, mrb_fixnum_value(WSLAY_MSG_MORE), mrb_symbol_value(mrb_intern_lit(mrb, "msg_more")));
-  mrb_hash_set(mrb, io_flags_hash, mrb_symbol_value(mrb_intern_lit(mrb, "msg_more")), mrb_fixnum_value(WSLAY_MSG_MORE));
+  mrb_hash_set(mrb, io_flags_hash, mrb_int_value(mrb, WSLAY_MSG_MORE), mrb_symbol_value(mrb_intern_lit(mrb, "msg_more")));
+  mrb_hash_set(mrb, io_flags_hash, mrb_symbol_value(mrb_intern_lit(mrb, "msg_more")), mrb_int_value(mrb, WSLAY_MSG_MORE));
 
   mrb_value wslay_opcode_hash = mrb_hash_new_capa(mrb, 6 * 2);
   mrb_define_const(mrb, wslay_mod, "OpCode", wslay_opcode_hash);
-  mrb_hash_set(mrb, wslay_opcode_hash, mrb_fixnum_value(WSLAY_CONTINUATION_FRAME), mrb_symbol_value(mrb_intern_lit(mrb, "continuation_frame")));
-  mrb_hash_set(mrb, wslay_opcode_hash, mrb_fixnum_value(WSLAY_TEXT_FRAME), mrb_symbol_value(mrb_intern_lit(mrb, "text_frame")));
-  mrb_hash_set(mrb, wslay_opcode_hash, mrb_fixnum_value(WSLAY_BINARY_FRAME), mrb_symbol_value(mrb_intern_lit(mrb, "binary_frame")));
-  mrb_hash_set(mrb, wslay_opcode_hash, mrb_fixnum_value(WSLAY_CONNECTION_CLOSE), mrb_symbol_value(mrb_intern_lit(mrb, "connection_close")));
-  mrb_hash_set(mrb, wslay_opcode_hash, mrb_fixnum_value(WSLAY_PING), mrb_symbol_value(mrb_intern_lit(mrb, "ping")));
-  mrb_hash_set(mrb, wslay_opcode_hash, mrb_fixnum_value(WSLAY_PONG), mrb_symbol_value(mrb_intern_lit(mrb, "pong")));
+  mrb_hash_set(mrb, wslay_opcode_hash, mrb_int_value(mrb, WSLAY_CONTINUATION_FRAME), mrb_symbol_value(mrb_intern_lit(mrb, "continuation_frame")));
+  mrb_hash_set(mrb, wslay_opcode_hash, mrb_int_value(mrb, WSLAY_TEXT_FRAME), mrb_symbol_value(mrb_intern_lit(mrb, "text_frame")));
+  mrb_hash_set(mrb, wslay_opcode_hash, mrb_int_value(mrb, WSLAY_BINARY_FRAME), mrb_symbol_value(mrb_intern_lit(mrb, "binary_frame")));
+  mrb_hash_set(mrb, wslay_opcode_hash, mrb_int_value(mrb, WSLAY_CONNECTION_CLOSE), mrb_symbol_value(mrb_intern_lit(mrb, "connection_close")));
+  mrb_hash_set(mrb, wslay_opcode_hash, mrb_int_value(mrb, WSLAY_PING), mrb_symbol_value(mrb_intern_lit(mrb, "ping")));
+  mrb_hash_set(mrb, wslay_opcode_hash, mrb_int_value(mrb, WSLAY_PONG), mrb_symbol_value(mrb_intern_lit(mrb, "pong")));
   mrb_value wslay_opcode_hash_keys = mrb_hash_keys(mrb, wslay_opcode_hash);
   for (mrb_int i = 0; i < RARRAY_LEN(wslay_opcode_hash_keys); i++) {
     mrb_value key = mrb_ary_ref(mrb, wslay_opcode_hash_keys, i);
